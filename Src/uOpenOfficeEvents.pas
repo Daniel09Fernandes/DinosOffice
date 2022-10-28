8b4bb81fa241518bd1cf4fa0b9dcbef156b92e79
Daniel Fernandes <danielfernandesroddrigues@gmail.com>
2022-10-28 18:53:53 -0300

    Update uOpenOfficeEvents.pas

--- Src/uOpenOfficeEvents.pas
+++ Src/uOpenOfficeEvents.pas
@@ -6,7 +6,7 @@
 { Developer: Daniel Fernandes Rodrigures }
 { Email    : danielfernandesroddrigues@gmail.com }
 { this unit is a part of the Open Source. }
-{ licensed under a MPL/GPL/LGPL three license - see LICENSE.md}
+{ licensed under a MIT - see LICENSE.md}
 
 { ******************************************************* }
 
