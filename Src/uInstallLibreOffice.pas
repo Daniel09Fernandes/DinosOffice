eee250a89a87d9d80be22292ab57243ab021bea7
Daniel09Fernandes <danielfernandesroddrigues@gmail.com>
2022-11-21 12:14:51 -0300

    Instalador da versão mais recente do libre office, caso não tenha instalado

---
+++ Src/uInstallLibreOffice.pas
@@ -0,0 +1,84 @@
+unit uInstallLibreOffice;
+
+interface
+
+uses  SysUtils, Variants, Classes, Vcl.OleCtrls, SHDocVw, Vcl.StdCtrls, Vcl.ExtCtrls;
+
+Type
+  TInstallLibreOffice = class(TComponent)
+  private
+    const
+      DonwloadClickJS = ' document.getElementsByClassName("dl_download_link")[0].click(); ';
+      URL = 'https://www.libreoffice.org/download/download-libreoffice/';
+    var
+      URL_download :string;
+      FWebBrowser : TWebBrowser;
+      FPageReadyForDownload : boolean;
+    procedure download;
+    procedure  WhenDocIsCompleted(ASender: TObject; const pDisp: IDispatch; const URL: OleVariant);
+  public
+     procedure DownloadLibreOffice;
+     constructor Create;
+     destructor Destroy;
+  end;
+
+
+implementation
+
+uses
+  Vcl.Controls, MSHTML, Vcl.Forms;
+
+{ TInstallLibreOffice }
+
+constructor TInstallLibreOffice.Create;
+begin
+  URL_download := 'https://www.libreoffice.org/donate/dl/win-x86_64/versao/pt-BR/LibreOffice_versao_Win_x64.msi';
+
+  if not assigned(FWebBrowser) then
+    FWebBrowser := TWebBrowser.Create(self);
+end;
+
+destructor TInstallLibreOffice.Destroy;
+begin
+  FreeAndNil(FWebBrowser);
+end;
+
+procedure TInstallLibreOffice.download;
+begin
+   FWebBrowser.Navigate(URL_download);
+   FWebBrowser.OnDocumentComplete := nil;
+end;
+
+procedure TInstallLibreOffice.DownloadLibreOffice;
+begin
+  TWinControl(FWebBrowser).Name   := 'WebBrowser';
+  FWebBrowser.Silent := true;  //don't show JS errors
+  FWebBrowser.Visible:= false;  //visible...by default true
+  FWebBrowser.HandleNeeded;
+
+  FWebBrowser.Navigate(URL);
+  FWebBrowser.RegisterAsBrowser:= True;
+  FWebBrowser.OnDocumentComplete :=  WhenDocIsCompleted;
+  FWebBrowser.Top    := 0;
+  FWebBrowser.Left   := 0;
+  FWebBrowser.Height := 0;
+  FWebBrowser.Width  := 0;
+end;
+
+procedure TInstallLibreOffice.WhenDocIsCompleted(ASender: TObject; const pDisp: IDispatch; const URL: OleVariant);
+var
+ Doc: IHTMLDocument2;
+ body : string;
+ versao : string;
+ UrlDownload: string;
+begin
+  Doc := FWebBrowser.Document as IHTMLDocument2;
+  body :=   doc.body.innerText;
+  versao := Copy(body,pos('is available for the following operating systems/architectures',body)-6,6).Trim;
+  UrlDownload := URL_download;
+  UrlDownload := UrlDownload.Replace('versao',versao);
+  URL_download :=  UrlDownload;
+  download;
+end;
+
+end.
--- Src/uOpenOffice.pas
+++ Src/uOpenOffice.pas
@@ -3,51 +3,54 @@ unit uOpenOffice;
 interface
 
 uses ActiveX, System.Classes, Vcl.Dialogs, System.Variants, Windows,
-  uOpenOfficeSetPrinter, uOpenOfficeEvents;
+  uOpenOfficeSetPrinter, uOpenOfficeEvents, uInstallLibreOffice,
+  System.UITypes;
 
-  Type
-   TTypeOffice   = (TpCalc,TpWriter);
-   TTypeLanguage = (TpCalcPTbr,TpCalcEn,TpWriterPTbr,TpWriterEn);
+Type
+  TTypeOffice = (TpCalc, TpWriter);
+  TTypeLanguage = (TpCalcPTbr, TpCalcEn, TpWriterPTbr, TpWriterEn);
 
 Type
   TOpenOffice = class(TComponent)
   private
     FURlFile: string;
-    FSetPrinter         : TSetPrinter;
-    FOnBeforePrint      : TBeforePrint;
-    FOnBeforeCloseFile  : TBeforeCloseFile;
-    FOnAfterCloseFile   : TAfterCloseFile;
+    FSetPrinter: TSetPrinter;
+    FOnBeforePrint: TBeforePrint;
+    FOnBeforeCloseFile: TBeforeCloseFile;
+    FOnAfterCloseFile: TAfterCloseFile;
 
-    FOnAfterGetValue   : TAfterGetValue;
-    FOnBeforeGetValue  : TBeforeGetValue;
-    FOnAfterSetValue   : TAfterSetValue;
-    FOnBeforeSetValue  : TBeforeSetValue;
+    FOnAfterGetValue: TAfterGetValue;
+    FOnBeforeGetValue: TBeforeGetValue;
+    FOnAfterSetValue: TAfterSetValue;
+    FOnBeforeSetValue: TBeforeSetValue;
 
+    InstallLibreOffice: TInstallLibreOffice;
     procedure SetURlFile(const Value: string);
     procedure inicialization;
   protected
     { Protected declarations }
-    objCoreReflection,
-    objDesktop,
-    objServiceManager,
-    objDocument,
-    objSCalc,
-    objWriter,
-    objDispatcher,
-    objCell, Charts: OleVariant;
-    NewFile    : array[0..1] of string;
+    objCoreReflection, objDesktop, objServiceManager, objDocument, objSCalc,
+      objWriter, objDispatcher, objCell, Charts: OleVariant;
+    NewFile: array [0 .. 1] of string;
     function convertFilePathToUrlFile(aFilePath: string): string;
     Property SetPrinter: TSetPrinter read FSetPrinter write FSetPrinter;
     procedure LoadDocument(FileName: string = '');
   published
     property URlFile: string read FURlFile write SetURlFile;
-    property OnBeforePrint     : TBeforePrint      read FOnBeforePrint      write FOnBeforePrint;
-    property OnBeforeCloseFile : TBeforeCloseFile  read FOnBeforeCloseFile  write FOnBeforeCloseFile;
-    property OnAfterCloseFile  : TAfterCloseFile   read FOnAfterCloseFile   write FOnAfterCloseFile;
-    property OnBeforeGetValue  : TBeforeGetValue   read FOnBeforeGetValue   write FOnBeforeGetValue;
-    property OnAfterGetValue   : TAfterGetValue    read FOnAfterGetValue    write FOnAfterGetValue;
-    property OnBeforeSetValue  : TBeforeSetValue   read FOnBeforeSetValue   write FOnBeforeSetValue;
-    property OnAfterSetValue   : TAfterSetValue    read FOnAfterSetValue    write FOnAfterSetValue;
+    property OnBeforePrint: TBeforePrint read FOnBeforePrint
+      write FOnBeforePrint;
+    property OnBeforeCloseFile: TBeforeCloseFile read FOnBeforeCloseFile
+      write FOnBeforeCloseFile;
+    property OnAfterCloseFile: TAfterCloseFile read FOnAfterCloseFile
+      write FOnAfterCloseFile;
+    property OnBeforeGetValue: TBeforeGetValue read FOnBeforeGetValue
+      write FOnBeforeGetValue;
+    property OnAfterGetValue: TAfterGetValue read FOnAfterGetValue
+      write FOnAfterGetValue;
+    property OnBeforeSetValue: TBeforeSetValue read FOnBeforeSetValue
+      write FOnBeforeSetValue;
+    property OnAfterSetValue: TAfterSetValue read FOnAfterSetValue
+      write FOnAfterSetValue;
   public
     procedure print;
     procedure CloseFile;
@@ -65,12 +68,12 @@ uses
 
 procedure TOpenOffice.CloseFile;
 begin
- if Assigned( FOnBeforeCloseFile) then
+  if Assigned(FOnBeforeCloseFile) then
     FOnBeforeCloseFile(self);
 
   objDocument.close(True);
 
-  if Assigned( FOnAfterCloseFile) then
+  if Assigned(FOnAfterCloseFile) then
     FOnAfterCloseFile(self);
 end;
 
@@ -88,8 +91,8 @@ procedure TOpenOffice.SetURlFile(const Value: string);
 begin
   FURlFile := Value;
 
-  if FURlFile.Trim.IsEmpty or (FURlFile = NewFile[integer(TpCalc)] )
-                           or (FURlFile = NewFile[integer(TpWriter)]) then
+  if FURlFile.Trim.IsEmpty or (FURlFile = NewFile[integer(TpCalc)]) or
+    (FURlFile = NewFile[integer(TpWriter)]) then
     exit;
 
   FURlFile := convertFilePathToUrlFile(FURlFile);
@@ -100,7 +103,7 @@ begin
   inherited;
   inicialization;
   FSetPrinter := TSetPrinter.Create(nil);
-  NewFile[integer(TpCalc)]   := 'private:factory/scalc';
+  NewFile[integer(TpCalc)] := 'private:factory/scalc';
   NewFile[integer(TpWriter)] := 'private:factory/swriter';
 end;
 
@@ -114,6 +117,9 @@ begin
   objDocument := Unassigned;
   objSCalc := Unassigned;
   objCell := Unassigned;
+
+  if assigned(InstallLibreOffice) then
+    freeAndNil(InstallLibreOffice);
 end;
 
 procedure TOpenOffice.inicialization;
@@ -121,54 +127,68 @@ begin
   try
     // Libre office
     objServiceManager := CreateOleObject('com.sun.star.ServiceManager');
-    objCoreReflection := objServiceManager.createInstance('com.sun.star.reflection.CoreReflection');
-    objDesktop        := objServiceManager.createInstance('com.sun.star.frame.Desktop');
+    objCoreReflection := objServiceManager.createInstance
+      ('com.sun.star.reflection.CoreReflection');
+    objDesktop := objServiceManager.createInstance
+      ('com.sun.star.frame.Desktop');
   except
-    messageDlg('Erro(pt-Br):  Instale o LibreOffice para usar o sistema' + #13 +
-      #13 + 'Error(En)  :  install  the LibreOffice to use the system' +#13#13+
-      'Dowload in: https://www.libreoffice.org/download/download-libreoffice/' ,
-      TMsgDlgType.mtWarning, [TMsgDlgBtn.mbOK], 0);
+    if messageDlg('Erro(pt-Br):  Instale o LibreOffice para usar o sistema' +
+      #13 + #13 + 'Error(En)  :  install  the LibreOffice to use the system' +
+      #13#13 + 'Dowload in: https://www.libreoffice.org/download/download-libreoffice/'
+      + #13#13 + '(pt-Br) Deseja instalar a versão mais recente do LibreOffice?'
+      + #13 + '(En) Do you want to install the latest version of LibreOffice?',
+
+      TMsgDlgType.mtWarning, [TMsgDlgBtn.mbYes, TMsgDlgBtn.mbNo], 0) = mrYes
+    then
+    begin
+      InstallLibreOffice := TInstallLibreOffice.Create;
+      InstallLibreOffice.DownloadLibreOffice;
+    end;
   end;
 end;
 
 procedure TOpenOffice.LoadDocument(FileName: string = '');
 begin
   if FileName = '' then
-      FileName := '_blank';
+    FileName := '_blank';
 
-  objDocument := objDesktop.loadComponentFromURL(URlFile, FileName, 0, VarArrayOf([]));
+  objDocument := objDesktop.loadComponentFromURL(URlFile, FileName, 0,
+    VarArrayOf([]));
 end;
 
 procedure TOpenOffice.print;
-var PaperSize: variant;
-    printerProperties : array[0..3] of variant;
+var
+  PaperSize: variant;
+  printerProperties: array [0 .. 3] of variant;
 begin
 
-  if Assigned(onBeforePrint) then
+  if Assigned(OnBeforePrint) then
   begin
-    onBeforePrint(self, FSetPrinter);
+    OnBeforePrint(self, FSetPrinter);
 
     PaperSize := objServiceManager.Bridge_GetStruct('com.sun.star.awt.Size');
 
-    PaperSize.Width  := FSetPrinter.PaperSize_Width;
+    PaperSize.Width := FSetPrinter.PaperSize_Width;
     PaperSize.Height := FSetPrinter.PaperSize_Height;
 
-    PrinterProperties[0] := objServiceManager.Bridge_GetStruct('com.sun.star.beans.PropertyValue');
-    PrinterProperties[0].Name  :='Name';
-    PrinterProperties[0].Value := FSetPrinter.PrinterName;
+    printerProperties[0] := objServiceManager.Bridge_GetStruct
+      ('com.sun.star.beans.PropertyValue');
+    printerProperties[0].Name := 'Name';
+    printerProperties[0].Value := FSetPrinter.PrinterName;
 
+    printerProperties[1] := objServiceManager.Bridge_GetStruct
+      ('com.sun.star.beans.PropertyValue');
+    printerProperties[1].Name := 'PaperSize';
+    printerProperties[1].Value := PaperSize;
 
-    PrinterProperties[1] := objServiceManager.Bridge_GetStruct('com.sun.star.beans.PropertyValue');
-    PrinterProperties[1].Name  :='PaperSize';
-    PrinterProperties[1].Value := PaperSize;
+    printerProperties[2] := objServiceManager.Bridge_GetStruct
+      ('com.sun.star.beans.PropertyValue');
+    printerProperties[2].Name := 'Pages';
+    printerProperties[2].Value := FSetPrinter.Pages;
 
-    PrinterProperties[2] := objServiceManager.Bridge_GetStruct('com.sun.star.beans.PropertyValue');
-    PrinterProperties[2].Name  :='Pages';
-    PrinterProperties[2].Value := FSetPrinter.Pages;
+    objDocument.Printer := VarArrayOf(printerProperties);
 
-    objDocument.Printer :=  VarArrayOf(PrinterProperties);
-
-    objDocument.print(VarArrayOf(PrinterProperties));
+    objDocument.print(VarArrayOf(printerProperties));
   end
   else
     objDocument.print(VarArrayOf([]));
