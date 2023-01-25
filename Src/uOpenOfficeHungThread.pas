86d09b4f52a2f883a8697d1f45df45ecf9470ce9
t_daniel.rodrigues <daniel.rodrigues@terceiroslinx.com.br>
2023-01-25 19:53:53 -0300

    Issue-2 Add File Thread

---
+++ Src/uOpenOfficeHungThread.pas
@@ -0,0 +1,50 @@
+
+unit uOpenOfficeHungThread;
+
+interface
+
+uses
+  ActiveX, System.Variants,System.SysUtils,
+  Windows, System.SyncObjs, System.Classes;
+
+type
+  TOpenOfficeHungThread = class(TThread)
+  private
+    FProc: Tproc;
+    procedure ExeProcedureExternal;
+  public
+   property ExecProc: Tproc read FProc write FProc;
+   constructor Create;
+   destructor Destroy; override;
+   procedure Execute; override;
+ end;
+
+implementation
+
+uses
+  Vcl.Forms;
+
+constructor TOpenOfficeHungThread.Create;
+begin
+  inherited Create(true);
+end;
+
+destructor TOpenOfficeHungThread.Destroy;
+begin
+  inherited;
+end;
+
+procedure TOpenOfficeHungThread.Execute;
+var i: integer;
+begin
+  inherited;
+  Self.Queue(self.ExeProcedureExternal);
+end;
+
+procedure TOpenOfficeHungThread.ExeProcedureExternal;
+begin
+ if Assigned(FProc) then
+   FProc();
+end;
+
+end.
